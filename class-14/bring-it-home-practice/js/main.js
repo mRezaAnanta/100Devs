f03794218465fc055811110db40af347c96acc20
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2024-09-15 09:53:33 +0700

    feat(class-14): finished class-14

--- class-14/bring-it-home-practice/js/main.js
+++ class-14/bring-it-home-practice/js/main.js
@@ -1,14 +1,46 @@
 // *Variables*
 // Create a variable and console log the value
+let what = 7;
+console.log("what: " + what)
 
 // Create a variable, add 10 to it, and alert the value
+let nope = 110;
+nope = + 10
+console.log("nope: " + nope)
 
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
+let subFour = (a, b, c, d) => {
+  const sub = a + b + c + d;
+  console.log("subFour: " + sub);
+}
 
 // Create a function that divides one number by another and returns the remainder
+let modTwo = (a, b) => {
+  const mod = a % b
+  console.log("modTwo: " + mod)
+}
 
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
+let jumanji = (a, b) => {
+  const jum = a + b
+  if (jum >= 50) {
+    alert("Jumanji!")
+  }
+  console.log("jumanji: " + jum)
+}
 
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
+let zebra = (a, b, c) => {
+  const z = a * b * c;
+  if (z % 3 == 0) {
+    alert("Zebra!")
+  }
+  console.log("zebra: " + z)
+}
+
+subFour(1, 2, 3, 4);
+modTwo(4, 2);
+jumanji(10, 50);
+zebra(1, 2, 3);
--- class-14/simple-functions/js/main.js
+++ class-14/simple-functions/js/main.js
@@ -1,5 +1,19 @@
 //create a function that adds two numbers and alerts the sum
+let addFunc = (a, b) => {
+  return console.log(a + b);
+  // return alert(a + b);
+}
 
 //create a function that multiplys three numbers and console logs the product
+let multiply = (a, b, c) => {
+  return console.log(a * b * c);
+}
 
 //create a function that divides two numbers and returns the ???
+let divide = (a, b) => {
+  return console.log(a / b);
+}
+
+addFunc(1, 2)
+multiply(1, 2, 3)
+divide(5, 2)
