7cc4c8cd33bd3ddcb7a644cee08ee7388ee8067f
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2024-10-05 16:16:43 +0700

    feat(class-16): add more func

---
+++ class-16/Notes.md
@@ -0,0 +1,7 @@
+# Links
+
+- [The JS Way](https://github.com/bpesquet/thejsway)
+- [Eloquent Javascript](http://eloquentjavascript.net/3rd_edition/)
+- [Codeacademy: Intro to Javascript](https://www.codecademy.com/learn/introduction-to-javascript)
+- [Git and Github crash course](https://youtu.be/SWYqp7iY_Tc)
+- [Git from the bottom up](https://jwiegley.github.io/git-from-the-bottom-up/)
--- class-16/variable-fun-time/js/main.js
+++ class-16/variable-fun-time/js/main.js
@@ -1,18 +1,31 @@
 //--- Easy
 //create a variable and assign it a number
+let num = 1
 
 //minus 10 from that number
+num = num - 10
 
 //print that number to the console
+console.log("num: " + num)
 
 //--- Medium
 //create a variable that holds a value from the input
-
+let input = document.querySelector('input#danceDanceRevolution').value;
 //add 25 to that number
+input = parseInt(input) + 25
 
 //alert that number
+alert(input)
 
 //--- Hard
 //create a variable that holds the h1
+let h1 = document.querySelector('h1');
+console.log("input: " + input)
 
 //add an event listener to that element that console logs the sum of the two previous variables
+// h1.addEventListener("click", console.log("sum: " + (num + input)));
+h1.addEventListener("click", sum);
+
+function sum() {
+  console.log("sum: " + (num + input))
+}
