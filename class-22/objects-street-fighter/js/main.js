d242b984e833e7181ffad6fa578610947490efc3
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2024-11-26 19:23:32 +0700

    feat(class-22): add stuff

--- class-22/objects-street-fighter/js/main.js
+++ class-22/objects-street-fighter/js/main.js
@@ -1,4 +1,18 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
-function Fighter() {
-
+function MakeFighter(punch, kick, catchPhrase, specialMove) {
+  this.punch = punch
+  this.kick = kick
+  this.catchPhrase = catchPhrase
+  this.specialMove = specialMove
+  this.taunt = function () {
+    console.log(`You can't handle my ${this.catchPhrase}`)
+  }
+  this.winning = function () {
+    console.log(`HAHA! ${this.catchPhrase}`)
+  }
+  this.dash = function () {
+    console.log(`Whooop, missed me!`)
+  }
 }
+
+let ryu = new MakeFighter('high', 'high', 'get over here!', 'Hadouken')
