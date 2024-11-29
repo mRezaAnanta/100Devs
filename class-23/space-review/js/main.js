14edfaa32fceaef8bf153c818e2e0181af3b2953
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2024-11-29 15:19:49 +0700

    feat(class-23): add exercises

--- class-23/space-review/js/main.js
+++ class-23/space-review/js/main.js
@@ -1,12 +1,31 @@
 //Arrays
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
+let arr = [1, 2, 3, 4, 5, 9]
+alert(arr.reduce((prev, cur) => prev + cur))
 
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
+const isSquared = (arr) => {
+  return arr.map((x) => x ** 2)
+}
 
 //Create a function that takes string
 //Print the reverse of that string to the console
+let str = "string!"
+const reverseString = (string) => console.log([...string].reverse().join(''))
+reverseString(str)
 
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+const palindrome = (string) => {
+  // let arr = string.split('')
+  // let a = []
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (!a.includes(arr[i])) a.push(arr[i])
+  // }
+  // return (a.length == arr.length)
+  return string === string.split('').reverse().join('')
+}
+alert(palindrome("abcdefghij"))
+alert(palindrome("abccba"))
--- class-23/turbo-review/js/main.js
+++ class-23/turbo-review/js/main.js
@@ -1,13 +1,35 @@
 // *Variables*
 // Declare a variable and assign it to a sentance as a string. Alert if the sentance is a question
+let question = "Is this a question?"
+alert(question.endsWith("?"))
 
 //Declare a variable, assign it a string of multiple words, replace every "jr. dev" with "software engineer", and print it to the console
+let introduction = "Hi. My name is mRezaAnanta and i'm a jr. dev"
+console.log(introduction.replaceAll("jr. dev", "software engineer"))
 
 // *Functions*
 // Create a function that returns rock, paper, or scissors as randomly as possible
+const rand = () => {
+  let random = Math.random()
+  return random < 0.33 ? "rock" : random < 0.66 ? "paper" : "scissors"
+}
+console.log(rand())
 
 // *Conditionals*
 //Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
+const againstBot = (yourChoice) => {
+  let r = rand()
+  return r == "rock" && yourChoice == "paper" || r == "paper" && yourChoice == "scissors" || r == "scissors" && yourChoice == "rock" ? `Bot chooses ${r}. You win!` :
+    r == "rock" && yourChoice == "scissors" || r == "paper" && yourChoice == "rock" || r == "scissors" && yourChoice == "paper" ? `Bot chooses ${r}. The bot win!` :
+      `You both chosses ${r}. Draw!`
+}
+console.log(againstBot("paper"))
 
 //*Loops*
 //Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
+const choicesArray = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(againstBot(arr[i]))
+  }
+}
+choicesArray(["paper", "rock", "scissors"])
