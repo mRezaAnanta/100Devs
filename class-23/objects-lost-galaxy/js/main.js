d42bc261ccf4d00a1703f42650a2f30d557db2f5
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2024-11-30 10:28:59 +0700

    feat(class-23): add class-23 exercises

--- class-23/objects-lost-galaxy/js/main.js
+++ class-23/objects-lost-galaxy/js/main.js
@@ -1,1 +1,16 @@
 //Create a dog object that has four properties and three methods
+const Dog = {
+  color: "Black",
+  breed: "poodle",
+  legs: 4,
+  name: "Fido",
+  bark: function () {
+    console.log("WOOF")
+  },
+  jump: function () {
+    console.log("BLAST OFF")
+  },
+  fetch: function () {
+    console.log("ZOOOOOM")
+  }
+}
--- class-23/objects-tony-hawk-pro-skater/js/main.js
+++ class-23/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,18 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+function ProSkater(name, brand, skaterColor, specialMove) {
+  this.name = name
+  this.brand = brand
+  this.skaterColor = skaterColor
+  this.specialMove = specialMove
+  this.intro = function () {
+    console.log(`My name is ${this.name}`)
+  }
+  this.jump = function () {
+    console.log(`YEEEHAAAAW`)
+  }
+  this.tricks = function () {
+    console.log(`${this.specialMove} is unbeatable!`)
+  }
+}
+
+let malcolmX = new ProSkater("Malcolm X", "Nike", "Blue", "Fly High")
