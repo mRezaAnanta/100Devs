a6aef5bbf9e87a92811ee7ad6c670d23d66859ba
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2024-12-01 10:15:50 +0700

    feat(class-27): add class-27 exercises

--- class-27/local-storage-botScore-button/js/main.js
+++ class-27/local-storage-botScore-button/js/main.js
@@ -1,3 +1,12 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
+if (!localStorage.getItem('botScore')) {
+  localStorage.setItem('botScore', 0)
+}
+document.querySelector('button').addEventListener('click', addOne)
 
-
+function addOne() {
+  // document.querySelector('h2').value += 1
+  let num = Number(localStorage.getItem('botScore'))
+  num += 1
+  localStorage.setItem('botScore', num)
+}
--- class-27/objects-netflix-tv-shows/js/main.js
+++ class-27/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,20 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
-
-
+class NetflixShow {
+  constructor(name, actor, date, country) {
+    this.name = name
+    this.actor = actor
+    this.date = date
+    this.country = country
+  }
+  releaseWhen() {
+    let now = new Date()
+    console.log(now / 1000)
+  }
+  showData() {
+    console.log(`${this.name}, ${this.actor},${this.date},${this.country},`)
+  }
+  isReleasedYet() {
+    console.log(`Not Yet`)
+  }
+}
+let theTrunk = new NetflixShow('The Trunk', 'Gong Yoo', '29/11/2024', 'South Korea')
