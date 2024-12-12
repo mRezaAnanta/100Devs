95e8486f61d36fdd7a19f4eb562cb7a8dc8d745b
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2024-12-12 16:42:57 +0700

    feat(class-26): update nasa api

--- class-26/objects-nasa/js/main.js
+++ class-26/objects-nasa/js/main.js
@@ -1,1 +1,20 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+let apiKey = "3SPfsBByrCgNpiTjnqoS3HbYZzwYeILYQSiymNhE"
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch() {
+  const choice = document.querySelector('input').value
+  const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${choice}`
+
+  fetch(url)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data)
+      document.querySelector('h2').innerText = data.title
+      document.querySelector('h3').innerText = data.explanation
+      data.media_type === 'image' ? document.querySelector('img').src = data.url : document.querySelector('iframe').src = data.url
+    })
+    .catch(err => {
+      console.log(`error ${err}`)
+    });
+}
