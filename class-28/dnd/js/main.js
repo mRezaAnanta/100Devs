c071ba373db6f5eb4c7989de7ac1a0c865743713
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2024-12-22 18:44:24 +0700

    feat(class-28): finished dnd

--- class-28/dnd/js/main.js
+++ class-28/dnd/js/main.js
@@ -1,17 +1,32 @@
 //Example fetch using DnD5eAPI - place subclasses in ul
 document.querySelector('button').addEventListener('click', getFetch)
 
-function getFetch(){
+function getFetch() {
   const choice = document.querySelector('input').value
   const url = `https://www.dnd5eapi.co/api/spells/${choice}`
-
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-       
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data)
+      document.querySelector('h2').innerText = data.name
+      // document.querySelector('h3').innerText = data.classes[0].name
+      // document.querySelector('ul').innerText = ''
+      const ul = document.querySelector('ul')
+      while (ul.firstChild) {
+        ul.removeChild(ul.firstChild)
+      }
+      data.subclasses.forEach(element => {
+        const li = document.createElement('li')
+        li.textContent = element.name
+        document.querySelector('ul').appendChild(li)
       });
+      // data.classes.forEach(element => {
+      //     const li = document.createElement('li')
+      //     li.textContent = element.name
+      //     document.querySelector('ul').appendChild(li)
+      // });
+    })
+    .catch(err => {
+      console.log(`error ${err}`)
+    });
 }
-
