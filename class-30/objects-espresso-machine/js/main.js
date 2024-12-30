089828c54b41d1b0e0e57572390c8ab6d59795e0
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2024-12-30 17:13:29 +0700

    feat(class-30): finished objects-espresso-machine

--- class-30/objects-espresso-machine/js/main.js
+++ class-30/objects-espresso-machine/js/main.js
@@ -1,1 +1,20 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class MakeEspressoMachine {
+  constructor(brand, color, price, model) {
+    this.brand = brand
+    this.color = color
+    this.price = price
+    this.model = model
+  }
+  turnOn() {
+    console.log("Good day, I am now on!")
+  }
+  shutdown() {
+    console.log(`Bye ${this.model}`)
+  }
+  brew() {
+    console.log(`Good stuff coming your way`)
+  }
+}
+
+let gaggia = new MakeEspressoMachine('Gaggia', 'red', 600, 'Classic Pro')
